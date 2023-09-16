675fde4b5223100b8ddebc12efb08427a0ac405e
savvy chezhian <savvychez@gmail.com>
2023-09-15 19:07:51 -0500

    relocated some files

---
+++ Game.py
@@ -0,0 +1,14 @@
+import chess
+from chess import Move, Board
+
+
+class GameState():
+    def __init__():
+        
+
+class Agent():
+    def get_move(boardState: Board) -> Move:
+        pass
+
+    def log_info(boardState: Board):
+        pass
---
+++ playerAgent.py
@@ -0,0 +1,10 @@
+from chess import Move, Board
+
+class Agent():
+    def get_move(boardState: Board) -> Move:
+        pass
+
+    def log_info(boardState: Board):
+        pass
+
+    
