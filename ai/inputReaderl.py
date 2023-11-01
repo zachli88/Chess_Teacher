06159b5e2c995bcaec559c8503c96d55a876e7ee
Hursh Jha <51828164+pikachu13307@users.noreply.github.com>
2023-10-31 19:45:15 -0500

    hopefuly working neuralnet traininging - yet to test

--- ai/inputReaderl.py
+++ ai/inputReaderl.py
@@ -1,22 +1,109 @@
 import chess
 import chess.pgn as pgn
+import torch
+import torch.nn as nn
+import ai.ai as ai
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
 
-pgn_file = open("temp.pgn") 
-game = pgn.read_game(pgn_file)
+# Define your input size
+input_size = 189
 
-while (game != None) :
+# Create a custom neural network class
+class SimpleNN(nn.Module):
+    def __init__(self):
+        super(SimpleNN, self).__init()
+        self.hidden_layer = nn.Sequential(
+            nn.Linear(input_size, 1),  # One neuron in the hidden layer
+            nn.ReLU()
+        )
+    def forward(self, x):
+        x = self.hidden_layer(x)
+        return x
+
+def train_net(input_arr, eval_arr):
+    input_tensor = torch.tensor(input_arr)
+    eval_tensor = torch.tensor(eval_arr)
+    criterion = nn.MSELoss()
+
+    dataset = TensorDataset(input_tensor, eval_tensor)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+    num_epochs = 1
+    for epoch in range(num_epochs):
+        for inputs, targets in dataloader:
+            optim.zero_grad()  # Zero the gradients
+            outputs = SimpleNN(inputs)  # Forward pass
+            loss = criterion(outputs, targets)  # Calculate the loss
+            loss.backward()  # Backpropagate
+            optim.step()  # Update model weights
+
+        # Optionally, you can print the loss for each epoch
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item()}')
 
 
+
+def neural_net_input(board):
+    piece_index = {"p": 5, "n": 0, "b":1, "r":2, "q":3, "k": 4}
+    inputs = [0] * 189
+    current_player = board.turn
+    for i in range (8):
+        for j in range (8):
+            square = chess.SQUARES[chess.square(i,j)]
+            piece = str(board.piece_at(square))
+            color = 1
+            if piece == piece.lower():
+                color *= -1
+            piece = piece.lower()
+            rank = j
+            if color == -1:
+                rank = 7 - j
+            file = i
+            if file > 3:
+                file = 7 - i
+            if piece != "none":
+                index = len(inputs) - 1 - piece_index[piece]
+                inputs[index] += color
+                if piece != "p":
+                    inputs[piece_index[piece] * 32 + rank * 4 + file] += color
+                else:
+                    inputs[piece_index[piece] * 32 + (rank - 1) * 4 + file] += color
+    return inputs
+
+def main():
     input_params = 0
-    board = chess.Board()
-    for number, move in enumerate(game.mainline_moves()):
-        board.push(move)
-        input_params+=1
-        if input_params == 1000:
-            #neuralnet(bullshit)
-            input_params -= 1000
-    
-    fen = board.fen()
-    print(fen)
-    print(board)
+    pgn_file = open("temp.pgn") 
     game = pgn.read_game(pgn_file)
+    pos_list = []
+    eval_list = []
+    while (game != None) :
+        board = chess.Board()
+        for number, move in enumerate(game.mainline_moves()):
+            board.push(move)
+                # val = str(board)
+                # #first number is file second number is rank (0,0) = A1
+                # square = chess.SQUARES[chess.square(0,0)]
+            net_inputs = neural_net_input(board)
+            best_move = ai.getMove(board.fen())
+            board_eval = ai.getEval()
+
+            if board_eval[0] == "m":
+                continue
+            pos_list.append(net_inputs)
+            eval_list.append(board_eval)
+
+            input_params+=1
+            if input_params == 1000:
+                #neuralnet(net_inputs) 
+                input_params -= 1000
+        fen = board.fen()
+        # print(fen)
+        # print(board)
+        game = pgn.read_game(pgn_file)
+
+    train_net(pos_list, eval_list)
+    
+
+
+
+if __name__ == "__main__":
+    main()
