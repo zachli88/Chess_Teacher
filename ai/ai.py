e72af79511987b97597a25a14097cf69be7405db
Hursh Jha <51828164+pikachu13307@users.noreply.github.com>
2023-11-03 19:55:51 -0500

    added neural net training fix, *ai depth changed to 1 to make faster evaluation for net training*

--- ai/ai.py
+++ ai/ai.py
@@ -4,11 +4,11 @@ from sys import platform
 
 stockfish = None
 if platform == "win32":
-    stockfish = Stockfish("./lib/stockfish-windows-x86-64-modern.exe")
+    stockfish = Stockfish("C:\\Users\\hursh\Desktop\\ECLAIR\\Chess_Teacher\\ai\\stockfish-windows-x86-64.exe")
 else: 
     stockfish = Stockfish("./lib/stockfish")
-# stockfish = Stockfish("/usr/local/Cellar/stockfish/15.1/bin/stockfish")
-stockfish.set_depth(20)
+# stockfish = Stockfish("/usr/local/Cellar/stockfish/15.1/bin/stockfish")Chess_Teacher/ai/stockfish-windows-x86-64.exe
+stockfish.set_depth(1)
 stockfish.set_skill_level(0)
 
 def getMove(fen):
